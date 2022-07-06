96af9f83f54a539df9e1ee503fa786890f3e26bc
rajasekhara <rajasekhara@toucan-l003>
2022-07-06 09:19:33 +0530

    comit string

--- assesment-3/src/Strings/StringsBothAreSame.java
+++ assesment-3/src/Strings/StringsBothAreSame.java
@@ -6,20 +6,19 @@ public class StringsBothAreSame
 	{
 	String s1="keep";
 	String s2="peek";
-	int c=0;
+	int count=0;
 	for(int i=0; i<s1.length(); i++)
 	{
 	   for(int j=0; j<s2.length(); j++)
 	   {
 	      if((s1.charAt(i))==(s2.charAt(j)))
 	       {
-	       c++;
-	       break;
-	       
+	       count++;
+	       break;       
 	       }
 	    }
 	}
-	if(c==s1.length() || c==s2.length())
+	if(count==s1.length() || count==s2.length())
 	{
 	System.out.print("true");
 	}
